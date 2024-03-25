37afd1cc765a195b9201117ab16cb42fd4b8b87a
unknown <wjdrbeks1021@naver.com>
2024-03-26 00:20:50 +0900

    multiprocessor

--- get_formula_from_arxiv_html.py
+++ get_formula_from_arxiv_html.py
@@ -4,52 +4,15 @@ from bs4 import BeautifulSoup
 import csv
 import pandas as pd
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# 32 math categories
-math_categories = [
-    "math.AG",
-    "math.AT",
-    "math.AP",
-    "math.CT",
-    "math.CA",
-    "math.CO",
-    "math.AC",
-    "math.CV",
-    "math.DG",
-    "math.DS",
-    "math.FA",
-    "math.GM",
-    "math.GN",
-    "math.GT",
-    "math.GR",
-    "math.HO",
-    "math.IT",
-    "math.KT",
-    "math.LO",
-    "math.MP",
-    "math.MG",
-    "math.NT",
-    "math.NA",
-    "math.OA",
-    "math.OC",
-    "math.PR",
-    "math.QA",
-    "math.RT",
-    "math.RA",
-    "math.SP",
-    "math.ST",
-    "math.SG",
-]
 
 # Given category of arXiv, get arxiv paper id and output list.
-def get_arxiv_papers_link(category="math.AG", max_papers=5, save_dir="./arxiv_papers"):
+def get_arxiv_papers_link(category, max_papers=500, year="24", month="03" ):
     html_link = []
     base_url = "https://arxiv.org"
-    category_url = f"{base_url}/list/{category}/current"
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
+    category_url = f"{base_url}/list/{category}/{year}{month}?show=1000"
+    print(category_url)
     try:
         response = requests.get(category_url)
         if response.status_code != 200:
@@ -74,7 +37,7 @@ def get_arxiv_papers_link(category="math.AG", max_papers=5, save_dir="./arxiv_pa
 
 # If we get id of paper, get mathematics formula from html file
 def get_math_expressions(arxiv_id, file_name='math_expressions.csv'):
-    # HTML 파일의 URL
+    # HTML  URL
     url = f'https://arxiv.org/html/{arxiv_id}'
 
 
@@ -101,36 +64,88 @@ def get_math_expressions(arxiv_id, file_name='math_expressions.csv'):
     else:
         return -1
 
-# get formula in csv.
-    # 32 * 500 = 16000 papers
-count = 0
-error_count = 0
-for categ in math_categories:
-    for id in get_arxiv_papers_link(category=categ, max_papers=500, save_dir="./arxiv_papers"):
-        try:
-            get_math_expressions(id, file_name=f"csv_files/{id}_tex.csv")
-            count = count + 1
-            print(count, "/ 16000")
-        except Exception as e:
-            count = count + 1
-            error_count = error_count +1
-            print(count, "/ 16000")
-            continue
-print(f"complete, error:{error_count}  complete:{16000-error_count}")
+def main():
+    # 32 math categories
+    math_categories = ['cs.HC',
+  'cs.IR',
+  'cs.IT',
+  'cs.LG',
+  'cs.LO',
+  'cs.MA',
+  'cs.MM',
+  'cs.MS',
+  'cs.NA',
+  'cs.NE',
+  'cs.NI',
+  'cs.OH',
+  'cs.OS',
+  'cs.PF',
+  'cs.PL',
+  'cs.RO',
+  'cs.SC',
+  'cs.SD',
+  'cs.SE',
+  'cs.SI',
+  'cs.SY']
+    #start year, month
+    year = 24
+    month = 3
+
+    #end year, month
+    end_year = 20
+    end_month = 3
+
+   
+    if not os.path.exists('csv_files_cs_2'):
+        os.makedirs('csv_files_cs_2')
+
+    # ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=24) as executor:
+        # Future
+        futures = [executor.submit(process_category, category, year, month, end_year, end_month) for category in math_categories]
+
+       
+        for future in as_completed(futures):
+            print(future.result())  
+
+def process_category(category, year, month, end_year, end_month):
+    count = 0
+    while True:
+        for id in get_arxiv_papers_link(category=category, max_papers=500, year=f"{year:02d}", month=f"{month:02d}"):
+            try:
+                get_math_expressions(id, file_name=f"csv_files_cs_2/{id}_tex.csv")
+                count = count + 1
+                print(count)
+            except Exception as e:
+                count = count + 1
+                print(count)
+                continue
+        
+        if year == end_year and month == end_month:
+            break
+        
+        month -= 1
+        
+        if month < 1:
+            year -= 1
+            month = 12
+
+    print("complete")
             
 
-
+if __name__ == "__main__":
+    main()
 
 
 
 # combine in one csv file.
-folder_path = 'csv_files'
+folder_path = 'csv_files_cs_2'
 csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
 tex_data = []
 for file in csv_files:
     file_path = os.path.join(folder_path, file)
-    df = pd.read_csv(file_path, usecols=['TeX'])  # 'TeX' 열만 읽어옵니다.
+    df = pd.read_csv(file_path, usecols=['TeX'])  
     tex_data.append(df)
 
 combined_df = pd.concat(tex_data).drop_duplicates().reset_index(drop=True)
